06b733e7c10a0b1f368b3b42188a2f8a7493a57a
yihaiyang <164144398@qq.com>
2022-07-20 17:44:48 +0800

    update

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+ 
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+ 
+@Controller
+public class TestController{
+ 
+    @RequestMapping("/hello")
+    public String getPage(HashMap<String, String> map){
+        map.put("title", "欢迎来到Spring Boot!");
+        return "/index";
+    }
+}
